ac1dd9fc2ce9da83bac6af9cc613e549fb5e0e86
David Espinoza <deerhou@gmail.com>
2024-03-20 15:21:36 -0500

    test html geo api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { GeolocationComponent } from './components/geolocation/geolocation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, GeolocationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'geolochtml';
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (!navigator.geolocation) {
+  alert('La Geolocation no es soportada en este navegador');
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
