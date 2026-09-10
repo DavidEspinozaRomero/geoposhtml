ae3dbea45f134580ba51d55800298d59373facad
David E <deerhou@gmail.com>
2026-09-10 17:55:31 -0500

    feat: update router configuration to use hash location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,17 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { authInterceptor } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideClientHydration(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideAnimations(),
   ],
 };
