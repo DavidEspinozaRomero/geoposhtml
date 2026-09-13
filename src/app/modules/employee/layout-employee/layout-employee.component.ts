d4148d2cd61977426ded54bee64f967d201e1d13
David E <deerhou@gmail.com>
2026-09-13 11:13:50 -0500

    refactor(employee:layout): migrate sidebar to tailwind

--- src/app/modules/employee/layout-employee/layout-employee.component.ts
+++ src/app/modules/employee/layout-employee/layout-employee.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, TitleCasePipe } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LucideApple, LucideDynamicIcon, LucideLogOut, type LucideIcon } from '@lucide/angular';
@@ -8,7 +8,7 @@ import { iconMap } from '../../../shared/ui/icon-map';
 @Component({
   selector: 'app-layout-employee',
   standalone: true,
-  imports: [RouterModule, NgClass, TitleCasePipe, LucideDynamicIcon, LucideApple, LucideLogOut],
+  imports: [RouterModule, TitleCasePipe, LucideDynamicIcon, LucideApple, LucideLogOut],
   templateUrl: './layout-employee.component.html',
   styleUrl: './layout-employee.component.scss',
 })
